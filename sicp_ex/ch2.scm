d8ec67d1dabf612677f9c6de67606d39039d652c
pasiel <youyi251@126.com>
2017-10-28 20:30:18 +0800

    ch2

---
+++ sicp_ex/ch2.scm
@@ -0,0 +1,37 @@
+;; 2.7
+(define (make-center-interval m n)
+  (make-interval (- m n) (+ m n)))
+(define (make-interval a b)
+  (cons a b))
+(define (upper-bound interval)
+  (if (> (car interval) (cdr interval))
+      (car interval)
+      (cdr interval)))
+(define (lower-bound interval)
+  (if (< (car interval) (cdr interval))
+      (car interval)
+      (cdr interval)))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (mul-interval x y)
+  (make-interval (* (lower-bound x) (lower-bound y))
+                 (* (upper-bound x) (upper-bound y))))
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+;;
+(define (part1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+(define (part2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+                  (add-interval (div-interval one r1)
+                                (div-interval one r2)))))
